479e61d2e4bc2f60b330e1ef71a2701e75885279
Eric Denovellis <edeno@bu.edu>
2020-08-21 18:03:26 -0700

    Add plot to init

--- track_linearization/__init__.py
+++ track_linearization/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from track_linearization.core import get_linearized_position
-from track_linearization.utils import make_track_graph, plot_track_graph
+from track_linearization.utils import (make_track_graph, plot_graph_as_1D,
+                                       plot_track_graph)
