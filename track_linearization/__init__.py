e6344fc76abdc53c0741d646b9f8fe457eb66ea6
Eric Denovellis <edeno@bu.edu>
2020-09-11 09:49:42 -0700

    Add movie making function

--- track_linearization/__init__.py
+++ track_linearization/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from track_linearization.core import get_linearized_position
-from track_linearization.utils import (make_track_graph, plot_graph_as_1D,
-                                       plot_track_graph)
+from track_linearization.utils import (
+    make_actual_vs_linearized_position_movie, make_track_graph,
+    plot_graph_as_1D, plot_track_graph)
