58b07115848c839bea11a8225158eba9ba329004
Eric Denovellis <edeno@bu.edu>
2022-10-20 22:51:46 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TESTS_REQUIRE = ["pytest >= 2.7.1"]
 
 setup(
     name="track_linearization",
-    version="2.2.0",
+    version="2.3.0",
     license="MIT",
     description=(""),
     author="",
